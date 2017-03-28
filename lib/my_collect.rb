bb0c6e4cb615ba3ff7b8e47fe777b5cc5eb3578c
Eric Farber <eric.j.farber@gmail.com>
2017-03-28 00:35:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+require 'pry'
 
+def my_collect(array)
+    i = 0
+    results = []
+    while i < array.length
 
+        results << yield(array[i])
+        i = i + 1
+    end
+    results
+end
+
+# collection = ['ruby', 'javascript', 'python', 'objective-c']
+#
+# my_collect(collection) do |lang|
+#   lang.uppercase
+#   binding.pry
+# end
