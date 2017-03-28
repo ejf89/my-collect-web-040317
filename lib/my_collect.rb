2d5570017142446568e838b4150d9d28e5f4df54
Eric Farber <eric.j.farber@gmail.com>
2017-03-28 01:39:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,16 +4,8 @@ def my_collect(array)
     i = 0
     results = []
     while i < array.length
-
         results << yield(array[i])
         i = i + 1
     end
     results
 end
-
-# collection = ['ruby', 'javascript', 'python', 'objective-c']
-#
-# my_collect(collection) do |lang|
-#   lang.uppercase
-#   binding.pry
-# end
